c1dfab5122b3fc91c0c0d0ed8123f457e2a75015
Shoaib <shoaib015muradnagar@gmail.com>
2019-12-23 11:57:13 +0530

    updated portfolio

---
+++ index.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function(){
+  if (window.outerWidth <= 800) {
+    document.querySelector('#carouselExampleControls').style.animationPlayState = 'running' ;
+    document.querySelector('#connect-section').style.animationPlayState = 'running' ;
+    document.querySelector('#skill-head').style.animationPlayState = 'running' ;
+  }
+
+  else {
+    document.addEventListener('scroll', function(){
+      document.querySelector('#carouselExampleControls').style.animationPlayState = 'running' ;
+      document.querySelector('#connect-section').style.animationPlayState = 'running' ;
+      document.querySelector('#skill-head').style.animationPlayState = 'running' ;
+    })
+  }
+
+})
